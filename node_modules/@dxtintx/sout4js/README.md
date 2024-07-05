4c798223085904bd474d0565a9d688bfeb6f4909
dxtintx7 <93837399+dotinto@users.noreply.github.com>
2024-07-05 22:35:21 +0300

    Update

---
+++ node_modules/@dxtintx/sout4js/README.md
@@ -0,0 +1,22 @@
+# sout4js
+
+Package for convenient logging of any information to the console with Log4j format for JavaScript.
+
+### Usage
+sout4js contains only 12 text output methods: 6 regular ones and the same 6, but with a larger number of arguments (Adv).
+
+`trace(message)`  
+`debug(message)`  
+`info(message)`  
+`warn(message)`  
+`error(message)`  
+`fatal(message)`  
+
+
+
+`traceAdv(message, thread, module)`  
+`debugAdv(message, thread, module)`  
+`infoAdv(message, thread, module)`  
+`warnAdv(message, thread, module)`  
+`errorAdv(message, thread, module)`  
+`fatalAdv(message, thread, module)`  
---
+++ node_modules/@dxtintx/sout4js/index.js
@@ -0,0 +1,48 @@
+function dateFormat() {
+    return `${new Date().getFullYear()}-${
+        (new Date().getMonth() < 10) ? "0" + new Date().getMonth() : new Date().getMonth()
+    }-${
+        (new Date().getDay() < 10) ? "0" + new Date().getDay() : new Date().getDay()
+    } ${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}.${new Date().getMilliseconds()}`
+}
+
+module.exports = {
+    // simple methods
+    trace(message) {
+        console.log(`${dateFormat()} \x1b[30mTRACE\x1b[0m - ${message}`)
+    },
+    debug(message) {
+        console.log(`${dateFormat()} \x1b[34mDEBUG\x1b[0m - ${message}`)
+    },
+    info(message) {
+        console.log(`${dateFormat()} \x1b[32mINFO\x1b[0m - ${message}`)
+    },
+    warn(message) {
+        console.log(`${dateFormat()} \x1b[33mWARN\x1b[0m - ${message}`)
+    },
+    error(message) {
+        console.log(`${dateFormat()} \x1b[31mERROR\x1b[0m - ${message}`)
+    },
+    fatal(message) {
+        console.log(`${dateFormat()} \x1b[31mFATAL\x1b[0m - ${message}`)
+    },
+    // advanced methods
+    traceAdv(message, thread, module) {
+        console.log(`${dateFormat()} [${thread}] \x1b[30mTRACE\x1b[0m ${module} - ${message}`)
+    },
+    debugAdv(message, thread, module) {
+        console.log(`${dateFormat()} [${thread}] \x1b[34mDEBUG\x1b[0m ${module} - ${message}`)
+    },
+    infoAdv(message, thread, module) {
+        console.log(`${dateFormat()} [${thread}] \x1b[32mINFO\x1b[0m ${module} - ${message}`)
+    },
+    warnAdv(message, thread, module) {
+        console.log(`${dateFormat()} [${thread}] \x1b[33mWARN\x1b[0m ${module} - ${message}`)
+    },
+    errorAdv(message, thread, module) {
+        console.log(`${dateFormat()} [${thread}] \x1b[31mERROR\x1b[0m ${module} - ${message}`)
+    },
+    fatalAdv(message, thread, module) {
+        console.log(`${dateFormat()} [${thread}] \x1b[31mFATAL\x1b[0m ${module} - ${message}`)
+    },
+}
