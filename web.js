36b10b769e2612a191b560e521d6d659cbf60a15
dxtintx7 <93837399+dotinto@users.noreply.github.com>
2024-07-09 21:16:40 +0300

    Used internal console methods & Added a few spaces

--- web.js
+++ web.js
@@ -17,37 +17,48 @@ const sout4js = {
     trace(message) {
         console.trace(`${dateFormat()} \x1b[30;47mTRACE\x1b[0m - ${message}`)
     },
+
     debug(message) {
         console.debug(`${dateFormat()} \x1b[34mDEBUG\x1b[0m - ${message}`)
     },
+
     info(message) {
         console.info(`${dateFormat()} \x1b[32mINFO \x1b[0m - ${message}`)
     },
+
     warn(message) {
         console.warn(`${dateFormat()} \x1b[33mWARN \x1b[0m - ${message}`)
     },
+
     error(message) {
         console.error(`${dateFormat()} \x1b[31mERROR\x1b[0m - ${message}`)
     },
+
     fatal(message) {
         console.error(`${dateFormat()} \x1b[31mFATAL\x1b[0m - ${message}`)
     },
+
     // advanced methods
     traceAdv(message, thread, module) {
         console.trace(`${dateFormat()} [${thread}] \x1b[30;47mTRACE\x1b[0m ${module} - ${message}`)
     },
+
     debugAdv(message, thread, module) {
         console.debug(`${dateFormat()} [${thread}] \x1b[34mDEBUG\x1b[0m ${module} - ${message}`)
     },
+
     infoAdv(message, thread, module) {
         console.info(`${dateFormat()} [${thread}] \x1b[32mINFO \x1b[0m ${module} - ${message}`)
     },
+
     warnAdv(message, thread, module) {
         console.warn(`${dateFormat()} [${thread}] \x1b[33mWARN \x1b[0m ${module} - ${message}`)
     },
+
     errorAdv(message, thread, module) {
         console.error(`${dateFormat()} [${thread}] \x1b[31mERROR\x1b[0m ${module} - ${message}`)
     },
+    
     fatalAdv(message, thread, module) {
         console.fatal(`${dateFormat()} [${thread}] \x1b[31mFATAL\x1b[0m ${module} - ${message}`)
     },
