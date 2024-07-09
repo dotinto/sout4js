9453b05613958a3f29eb4d870b472653771af0e1
dxtintx7 <93837399+dotinto@users.noreply.github.com>
2024-07-09 15:27:30 +0300

    Use internat console methods

--- web.js
+++ web.js
@@ -15,40 +15,40 @@ function dateFormat() {
 const sout4js = {
     // simple methods
     trace(message) {
-        console.log(`${dateFormat()} \x1b[30;47mTRACE\x1b[0m - ${message}`)
+        console.trace(`${dateFormat()} \x1b[30;47mTRACE\x1b[0m - ${message}`)
     },
     debug(message) {
-        console.log(`${dateFormat()} \x1b[34mDEBUG\x1b[0m - ${message}`)
+        console.debug(`${dateFormat()} \x1b[34mDEBUG\x1b[0m - ${message}`)
     },
     info(message) {
-        console.log(`${dateFormat()} \x1b[32mINFO \x1b[0m - ${message}`)
+        console.info(`${dateFormat()} \x1b[32mINFO \x1b[0m - ${message}`)
     },
     warn(message) {
-        console.log(`${dateFormat()} \x1b[33mWARN \x1b[0m - ${message}`)
+        console.warn(`${dateFormat()} \x1b[33mWARN \x1b[0m - ${message}`)
     },
     error(message) {
-        console.log(`${dateFormat()} \x1b[31mERROR\x1b[0m - ${message}`)
+        console.error(`${dateFormat()} \x1b[31mERROR\x1b[0m - ${message}`)
     },
     fatal(message) {
-        console.log(`${dateFormat()} \x1b[31mFATAL\x1b[0m - ${message}`)
+        console.error(`${dateFormat()} \x1b[31mFATAL\x1b[0m - ${message}`)
     },
     // advanced methods
     traceAdv(message, thread, module) {
-        console.log(`${dateFormat()} [${thread}] \x1b[30;47mTRACE\x1b[0m ${module} - ${message}`)
+        console.trace(`${dateFormat()} [${thread}] \x1b[30;47mTRACE\x1b[0m ${module} - ${message}`)
     },
     debugAdv(message, thread, module) {
-        console.log(`${dateFormat()} [${thread}] \x1b[34mDEBUG\x1b[0m ${module} - ${message}`)
+        console.debug(`${dateFormat()} [${thread}] \x1b[34mDEBUG\x1b[0m ${module} - ${message}`)
     },
     infoAdv(message, thread, module) {
-        console.log(`${dateFormat()} [${thread}] \x1b[32mINFO \x1b[0m ${module} - ${message}`)
+        console.info(`${dateFormat()} [${thread}] \x1b[32mINFO \x1b[0m ${module} - ${message}`)
     },
     warnAdv(message, thread, module) {
-        console.log(`${dateFormat()} [${thread}] \x1b[33mWARN \x1b[0m ${module} - ${message}`)
+        console.warn(`${dateFormat()} [${thread}] \x1b[33mWARN \x1b[0m ${module} - ${message}`)
     },
     errorAdv(message, thread, module) {
-        console.log(`${dateFormat()} [${thread}] \x1b[31mERROR\x1b[0m ${module} - ${message}`)
+        console.error(`${dateFormat()} [${thread}] \x1b[31mERROR\x1b[0m ${module} - ${message}`)
     },
     fatalAdv(message, thread, module) {
-        console.log(`${dateFormat()} [${thread}] \x1b[31mFATAL\x1b[0m ${module} - ${message}`)
+        console.fatal(`${dateFormat()} [${thread}] \x1b[31mFATAL\x1b[0m ${module} - ${message}`)
     },
 }
