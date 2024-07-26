559599a27a0a5ff05034659e0900106bcb6032e8
dxtintx7 <93837399+dotinto@users.noreply.github.com>
2024-07-26 23:22:29 +0300

    Update types.d.ts

--- types.d.ts
+++ types.d.ts
@@ -7,14 +7,14 @@ declare namespace sout4js {
     function info(message: string): void;
     function warn(message: string): void;
     function error(message: string): void;
-
     function fatal(message: string): void;
+    
     function traceAdv(message: string, thread: string, module: string): void;
-    function debugAdv(message: string): void;
-    function infoAdv(message: string): void;
-    function warnAdv(message: string): void;
-    function errorAdv(message: string): void;
-    function fatalAdv(message: string): void;
+    function debugAdv(message: string, thread: string, module: string): void;
+    function infoAdv(message: string, thread: string, module: string): void;
+    function warnAdv(message: string, thread: string, module: string): void;
+    function errorAdv(message: string, thread: string, module: string): void;
+    function fatalAdv(message: string, thread: string, module: string): void;
 }
 
 export default sout4js;
